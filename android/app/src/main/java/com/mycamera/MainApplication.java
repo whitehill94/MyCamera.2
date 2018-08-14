da75faa2b85ad374693bba301001ce9dcd17c8a9
Jamie Whitehill <j.c.whitehill@gmail.com>
2018-08-14 15:13:23 +0300

    Adding the Camera_Roll_Functionality
    Adding the Camera_Roll_Functionality

--- android/app/src/main/java/com/mycamera/MainApplication.java
+++ android/app/src/main/java/com/mycamera/MainApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import cl.json.RNSharePackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSharePackage(),
+            new RNFetchBlobPackage(),
             
       );
       new RCTCameraPackage()
--- index.ios.js
+++ index.ios.js
@@ -4,80 +4,106 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  Dimensions,
-  View,
-  TouchableOpacity,
-  Image,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    Dimensions,
+    View,
+    TouchableOpacity,
+    Image,
+    CameraRoll,
+    ScrollView,
+    Button,
 } from 'react-native';
 
 
 import Camera from 'react-native-camera';
 import { createStackNavigator } from 'react-navigation'
 
-export default class MyCamera extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-      <Text> Viewfinder</Text>
-       <Camera
-           ref={(cam) => {
-             this.camera = cam;
-           }}
-           style={styles.preview}
-           aspect={Camera.constants.Aspect.fill}>
-          
-          <TouchableOpacity style={styles.button} onPress={this.takePicture.bind(this)}>
-          <Image source={require("./resources/images/shutter_button.png")}/>
-          </TouchableOpacity> 
-       </Camera>
-      </View>
-    );
-  }
 
-  takePicture() {
-   this.camera.capture()
-     .then((data) => console.log(data))
-     .catch(err => console.error(err));
- }
-}
+
+export default class MyCamera extends Component {
+    render() {
+
+        return (
+
+            <View style={styles.container}>
+
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}>
+
+                    <TouchableOpacity style={styles.button} onPress={this.takePicture.bind(this)}>
+                        <Image source={require("./resources/images/shutter_button.png")}/>
+                    </TouchableOpacity>
+
+                </Camera>
+
+                <Button title="Load Images" onPress={this._handleButtonPress}/>
+
+
+                {this.state.photos.map((p, i) => {
+                    <Image
+                        key={i}
+                        style={{
+                            width: 300,
+                            height: 100,
+                        }}
+                        source={{uri: p.node.image.uri}}
+                    /> // Closing IMG Tag
+
+                })}
+
+            </View>
+        ); //Closing Return 
+    } //Closing Render 
+
+    takePicture() {
+        this.camera.capture()
+            .then((data) => console.log(data))
+            .catch(err => console.error(err));
+    }// Closing takePicture
+
+    _handleButtonPress = () => {
+        CameraRoll.getPhotos({
+            first: 20,
+            assetType: 'Photos',
+        })
+            .then(r => {
+                this.setState({photos: r.edges});
+            })
+            .catch((err) => {
+                //Error Loading Images
+            });
+    }; //Closing handleButtonPress
+
+};// Closing MyCamera
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  preview: {
-   flex: 1,
-   justifyContent: 'flex-end',
-   alignItems: 'center',
-   height: Dimensions.get('window').height,
-   width: Dimensions.get('window').width
- },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    color: '#000',
-    paddingRight: 20,
-    margin: 40
-  }
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        color: '#000',
+        paddingRight: 20,
+        margin: 40
+    }
 });
-
-class splashScreen extends React.Component{
-  return(){
-    /** 
-
-    Additional Args..
-
-
-
-    */
-  }
-}
 
 AppRegistry.registerComponent('MyCamera', () => MyCamera);
